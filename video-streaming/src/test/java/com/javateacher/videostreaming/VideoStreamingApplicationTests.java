78a394c490d7227cf564a395f99dcd48d4b3dae5
Zekai Kırıktaş <zkkiriktass@gmail.com>
2023-09-23 19:05:24 +0300

    video streaming demo

---
+++ video-streaming/src/test/java/com/javateacher/videostreaming/VideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javateacher.videostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
